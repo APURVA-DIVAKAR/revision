566228da47fa1cd2635a072b9bbfac6aaefe2d9d
APURVA-DIVAKAR <apurvadiwaker@gmail.com>
2026-03-21 00:46:26 +0530

    NodeRevision

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+const http = require('http');
 
 const PORT = 3000;
 
@@ -15,7 +17,55 @@ const printFileData=()=>{
     })
 }
 
-printFileData();
+// printFileData();
+
+// 2.  Write a function that creates a new directory named "logs" and logs a message to a file named "info.log" inside the directory
+
+function createDirectory () {
+    const myData = "Reading data from info.log file";
+    fs.mkdir("./logs",{recursive:true},(err)=>{
+        if(err) throw err;
+        else{
+            fs.writeFile("./logs/info.log",myData,(err)=>{
+                if(err) throw err;
+            })
+        }
+    })
+}
+
+// createDirectory();
+
+// 3. Write a function that fetches data from an API endpoint using the Axios library and returns the response
+
+async function fecthData () {
+    try{
+        const res = await axios.get('https://random-data-api.com/api/v2/users')
+        return res;
+    } catch(err){
+        throw err;
+    }
+}
+
+// fecthData()
+// .then(data=>console.log(data))
+// .catch(err=>console.log(err))
+
+// 4. Write a function in Node.js that uses the native "http" module to create a simple HTTP server that listens on port 3000 and returns a "Hello, world!" message to any incoming request.
+
+http.createServer((req,res)=>{
+  res.end('Hello HTTP!!')
+}).listen(PORT);
+
+// 5. Write a function in Node.js that accepts a command-line argument and logs it to the console
+
+const getArgs = () =>{
+    let arg = process.argv.slice(2);
+    console.log(arg);
+}
+
+getArgs();
+// node index.js hello User
+
 
 
 app.listen(PORT,()=>{
